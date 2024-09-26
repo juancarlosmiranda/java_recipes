c245aba10fae6d08a753d3ba573c5b5740d07ae3
usuario <jmiranda@pol.una.py>
2024-09-26 17:25:21 -0400

    first commit

--- intro_java/src/main/java/com/mycompany/intro_java/MyFirstClassInJava.java
+++ intro_java/src/main/java/com/mycompany/intro_java/MyFirstClassInJava.java
@@ -10,12 +10,16 @@
  * 
  */
 package com.mycompany.intro_java;
+// it is another package
+import com.mycompany.intro_java.newpackage.NewClass;
 
 public class MyFirstClassInJava {
 
     public static void main(String[] args) {
         System.out.println("--------------------------------");
         System.out.println("It is my first programm in Java!");
-        System.out.println("--------------------------------");        
+        System.out.println("--------------------------------");
+        // calling an static method from other package
+        NewClass.method01();
     }
 }
--- intro_java/src/main/java/com/mycompany/intro_java/newpackage/NewClass.java
+++ intro_java/src/main/java/com/mycompany/intro_java/newpackage/NewClass.java
@@ -9,5 +9,12 @@ package com.mycompany.intro_java.newpackage;
  * @author usuario
  */
 public class NewClass {
-    
+    public static int method01(){
+        System.out.println("static int method01() --> ");
+        return 1;
+    }
+    public static int method02(){
+        System.out.println("static int method02() --> ");
+        return 1;
+    }
 }
